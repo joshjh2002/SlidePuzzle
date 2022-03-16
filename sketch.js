01ec87e30a4f5c20e71be84c596357f1b34837cf
yb012845 <yb012845@student.reading.ac.uk>
2022-03-16 15:12:04 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,135 @@
+const queryString = window.location.search;
+
+const urlParams = new URLSearchParams(queryString);
+
+const imageUrl = urlParams.get("image");
+const columns = urlParams.get("cols");
+const rowsUrl = urlParams.get("rows");
+
+let tiles;
+let cols;
+let rows;
+let w, h;
+// Loading the image
+function preload() {
+  if (imageUrl == null) source = loadImage("bmo.jpg");
+  else source = loadImage(imageUrl);
+
+  if (columns == null) cols = 4;
+  else cols = columns;
+
+  if (rowsUrl == null) rows = 4;
+  else rows = rowsUrl;
+}
+
+function setup() {
+  createCanvas(400, 400);
+  // pixel dimensions of each tiles
+  w = width / cols;
+  h = height / rows;
+  let index = 0;
+
+  tiles = new Array(cols);
+
+  // Chop up source image into tiles
+  for (let i = 0; i < cols; i++) {
+    tiles[i] = new Array(rows);
+    for (let j = 0; j < rows; j++) {
+      let img = createImage(w, h);
+      img.copy(source, i * w, j * h, w, h, 0, 0, w, h);
+      let tile = new Tile(index, img);
+      tiles[i][j] = tile;
+      index++;
+    }
+  }
+
+  tiles[cols - 1][rows - 1].index = -1;
+
+  shuffle_tiles();
+}
+
+function draw() {
+  background(0);
+  // Draw the current board
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let tile = tiles[i][j];
+      if (tile.index > -1) {
+        let img = tile.img;
+        image(img, i * w, j * h, w, h);
+      }
+    }
+  }
+
+  // Show it as grid
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      strokeWeight(2);
+      noFill();
+      rect(i * w, j * h, w, h);
+    }
+  }
+}
+
+// Swap two elements of an array
+function swap(i, j, a, b) {
+  //console.log(i + ", " + j + "\n" + a + ", " + b);
+  let temp = tiles[i][j];
+  tiles[i][j] = tiles[a][b];
+  tiles[a][b] = temp;
+}
+
+// Move based on click
+function mousePressed() {
+  let i = floor(mouseX / w);
+  let j = floor(mouseY / h);
+  if (i > cols - 1 || j > rows - 1) return;
+  move(i, j);
+}
+
+function move(i, j) {
+  let tile = tiles[i][j];
+  if (tile.index <= -1) return;
+
+  if (i + 1 < cols) {
+    //console.log(tiles[i + 1][j]);
+    if (tiles[i + 1][j].index <= -1) {
+      swap(i, j, i + 1, j);
+      return;
+    }
+  }
+
+  if (i - 1 >= 0) {
+    //console.log(tiles[i - 1][j]);
+    if (tiles[i - 1][j].index <= -1) {
+      swap(i, j, i - 1, j);
+      return;
+    }
+  }
+
+  if (j + 1 < rows) {
+    //console.log(tiles[i][j + 1]);
+    if (tiles[i][j + 1].index <= -1) {
+      swap(i, j, i, j + 1);
+      return;
+    }
+  }
+  if (j - 1 >= 0) {
+    //console.log(tiles[i][j - 1]);
+    if (tiles[i][j - 1].index <= -1) {
+      swap(i, j, i, j - 1);
+      return;
+    }
+  }
+}
+
+function shuffle_tiles() {
+  for (let i = 0; i < 100; i++) {
+    let i = floor(random() * (cols - 1));
+    let a = floor(random() * (cols - 1));
+
+    let j = floor(random() * (rows - 1));
+    let b = floor(random() * (rows - 1));
+    swap(i, j, a, b);
+  }
+}
